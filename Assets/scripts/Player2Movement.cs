8a2dd7b581909ad254573c85a3fd7acb3bd3f3e6
Unknown <tgr235@nyu.edu>
2017-02-28 02:06:11 -0500

    initial commit

---
+++ Assets/scripts/Player2Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour {
+
+	private float windThrust;
+	public Rigidbody rb;
+	public Transform target;
+	public float testhelper;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		windThrust = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.J)){
+			transform.localEulerAngles += new Vector3( 0f, -90f * Time.deltaTime, 0f );
+		}
+
+		if (Input.GetKey (KeyCode.L)){
+			transform.localEulerAngles += new Vector3( 0f, 90f * Time.deltaTime, 0f );
+		}
+
+		float windangle = Quaternion.Angle(transform.rotation, target.rotation);
+		Debug.Log (windangle);
+
+
+		if (windangle > 0 && windangle <= 90){
+			windThrust = windangle;
+		}
+		else if (windangle > 90 && windangle <= 180){
+			windThrust = Mathf.Abs (windangle - 180);
+		}
+		else if (windangle > 180 && windangle <= 270){
+			windangle = windangle - 10;
+			windThrust = Mathf.Abs (windangle - 180);
+		}
+		else if (windangle > 270 && windangle <= 360){
+			windangle = windangle - 10;
+			windThrust = Mathf.Abs (windangle - 360);
+		}
+
+		if (windThrust > 60){
+			windThrust = windThrust - 10;
+		}
+		rb.velocity = transform.forward * windThrust * Time.deltaTime * testhelper;
+	}
+}
---
+++ Assets/scripts/timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+	Text myText;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text>().text = "";
+		myText = GetComponent<Text>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		myText.text = Time.time.ToString("F3");
+	}
+
+}
